37e402abbfbc1f27fbd271aec45a7ebe7380de1f
ClemMnz <45999671+ClemMnz@users.noreply.github.com>
2018-12-19 22:46:18 +0100

    Add files via upload

---
+++ src/fr/eni_ecole/jdbc_dvd/dao/IFactureContract.java
@@ -0,0 +1,12 @@
+package fr.eni_ecole.jdbc_dvd.dao;
+
+public interface IFactureContract {
+
+public static final String TABLE_FACTURE = "FACTURES";
+	
+	public static final String COL_FACTURE_NUM="num_facture"; 
+	public static final String COL_FACTURE_CODE_CLI="code_client"; 
+	public static final String COL_FACTURE_DATEFACT="date_facture"; 
+
+	
+}
